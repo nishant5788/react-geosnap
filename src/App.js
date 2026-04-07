ad61e5613819b9bee88eaaf9271879d0744081c1
Nishant Sharma <nishants5788@gmail.com>
2026-04-08 03:44:25 +0800

    Refactor geolocation logic into a custom hook and update App component to use it

--- src/App.js
+++ src/App.js
@@ -1,50 +1,49 @@
 import { useState } from "react";
+import { useGeolocation } from "./useGeolocation";
+
+
 
 export default function App() {
+  const {
+    isLoading,
+    position: { lat, long },
+    getPosition,
+  } = useGeolocation();
 
-  const [isLoading, setIsLoading] = useState(false);
-  const [position, setPosition] = useState({});
+  const [clicks, setClicks] = useState(0);
 
-  const {lat, long} = position;
-
-  function getPosition() {
-    
-    setIsLoading(true);
-
-    navigator.geolocation.getCurrentPosition(
-      (pos) => {
-        setPosition({
-          lat: pos.coords.latitude,
-          long: pos.coords.longitude,
-        });
-      }
-    );
-    setIsLoading(false);
-
+  function handleClick() {
+    setClicks((count) => count + 1);
+    getPosition();
   }
 
   return (
-   <div class="container">
-    <h1>📍 GeoSnap</h1>
+    <div class="container">
+      <h1>📍 GeoSnap</h1>
 
-    <button onClick={getPosition} disabled={isLoading}>
-      Get My Position
-    </button>
+      <button onClick={handleClick} disabled={isLoading}>
+        Get My Position
+      </button>
 
-    <p class="loading hidden">Getting your location...</p>
+      {lat || long ? (
+        <div class="result">
+          <p>Your GPS position:</p>
+          <a
+            href={`https://www.openstreetmap.org/#map=16/${lat}/${long}`}
+            target="_blank"
+            rel="noreferrer"
+            class="map-link"
+          >
+            {lat}, {long}
+          </a>
+        </div>
+      ) : (
+        ""
+      )}
 
-    {position.lat || position.long ? <div class="result">
-      <p>Your GPS position:</p>
-      <a href={`https://www.openstreetmap.org/#map=16/${lat}/${long}`} target="_blank" class="map-link">
-        {lat}, {long}
-      </a>
-    </div> : ""}
-
-    
-
-    <p class="counter">
-      Requests: <span id="requestCount">0</span>
-    </p>
-  </div>
+      <p class="counter">
+        Requests: <span>{clicks}</span>
+      </p>
+    </div>
   );
 }
---
+++ src/useGeolocation.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+export function useGeolocation() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [position, setPosition] = useState({});
+  const { lat, long } = position;
+
+  function getPosition() {
+    setIsLoading(true);
+
+    navigator.geolocation.getCurrentPosition((pos) => {
+      setPosition({
+        lat: pos.coords.latitude,
+        long: pos.coords.longitude,
+      });
+    });
+    setIsLoading(false);
+  }
+
+  return { isLoading, position, getPosition };
+}
